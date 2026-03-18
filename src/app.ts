5c6bae970c48ee7d0ad6107a824eae3d140f2258
khaled <khaledmazen2002@gmail.com>
2026-03-18 05:08:24 +0200

    connect job routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import pipelineRoutes from "./routes/pipeline.routes.js"; 
 import webhookRoutes from "./routes/webhook.routes.js";
+import jobRoutes from "./routes/job.routes.js";
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use("/pipelines", pipelineRoutes);
 
 app.use("/webhooks", webhookRoutes);
+
+app.use("/jobs", jobRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server running 🚀");
