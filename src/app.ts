5ac319e4b82ea40f2197b254fce322772ce146d0
khaled <khaledmazen2002@gmail.com>
2026-03-18 02:05:51 +0200

    connect webhook routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import pipelineRoutes from "./routes/pipeline.routes.js"; 
+import webhookRoutes from "./routes/webhook.routes.js";
 
 const app = express();
 app.use(express.json());
 
 app.use("/pipelines", pipelineRoutes);
+
+app.use("/webhooks", webhookRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server running 🚀");
