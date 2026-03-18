eb21f274a20e234e79f0577bc1191a5cb253dbca
khaled <khaledmazen2002@gmail.com>
2026-03-19 01:28:16 +0200

    remove  in-memory stores after Drizzle migration

--- src/models/pipeline.store.ts
+++
@@ -1,6 +0,0 @@
-import type { Pipeline } from "./pipeline.model.js";
-
-export let pipelines: Pipeline[] = [];
-let currentPipelineId = 1;
-
-export const getNextPipelineId = () => currentPipelineId++;
