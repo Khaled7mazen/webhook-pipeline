5201fb6b87071de0461bfcbed91fdc3de01e3656
khaled <khaledmazen2002@gmail.com>
2026-03-18 23:30:14 +0200

    set up Drizzle ORM and PostgreSQL configuration

--- src/app.ts
+++ src/app.ts
@@ -19,3 +19,5 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
+
+//psql postgres://postgres:postgres@localhost:5432/postgres
--- src/controllers/worker.controller.ts
+++ src/controllers/worker.controller.ts
@@ -1,24 +1,24 @@
-import { jobs } from "../models/job.model.js";
-import { pipelines } from "./pipeline.controller.js";
-import { processData } from "./processor.controller.js";
-import { deliverJobs } from "./delivery.controller.js";
+// import { jobs } from "../models/job.model.js";
+// import { pipelines } from "./pipeline.controller.js";
+// import { processData } from "./processor.controller.js";
+// import { deliverJobs } from "./delivery.controller.js";
 
-export const processPendingJobs = async() => {
-  jobs.forEach(job => {
-    if (job.status === "pending") {
-      job.status = "processing";
+// export const processPendingJobs = async() => {
+//   jobs.forEach(job => {
+//     if (job.status === "pending") {
+//       job.status = "processing";
 
-      const pipeline = pipelines.find(p => p.id === job.pipelineId);
+//       const pipeline = pipelines.find(p => p.id === job.pipelineId);
 
-      if (pipeline) {
-        job.payload = processData(pipeline.action as any, job.payload);
-        job.status = "done";
-      } else {
-        job.status = "failed";
-      }
-    }
-  });
+//       if (pipeline) {
+//         job.payload = processData(pipeline.action as any, job.payload);
+//         job.status = "done";
+//       } else {
+//         job.status = "failed";
+//       }
+//     }
+//   });
 
-    await deliverJobs();
+//     await deliverJobs();
 
-};
+// };
